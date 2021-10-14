04b28718211f353de7e1dc6394ccce158723fd99
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-14 00:03:41 +0000

    Deploy elijahthomas1/comp3104_test to github.com/elijahthomas1/comp3104_test.git:gh-pages

---
+++ precache-manifest.ea507078c74fb8514c39aecb6080bb7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c306d59343d45f1bf2d",
+    "url": "/elijahthomas1/comp3104_test/static/css/main.a5a828a5.chunk.css"
+  },
+  {
+    "revision": "8c306d59343d45f1bf2d",
+    "url": "/elijahthomas1/comp3104_test/static/js/main.8c306d59.chunk.js"
+  },
+  {
+    "revision": "ebf2980c2c84060833e0",
+    "url": "/elijahthomas1/comp3104_test/static/js/runtime~main.ebf2980c.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/elijahthomas1/comp3104_test/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "55cd2ea3cb1ba031e4591bc8b34328d7",
+    "url": "/elijahthomas1/comp3104_test/index.html"
+  }
+];
